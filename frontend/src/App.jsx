45b9e46ba8348f6b610eac613e8aa79a638f415d
YeahitsDivyansh <divyansh972003@gmail.com>
2025-04-07 23:02:50 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-200">Hello React</h1>
+      <Button variant="default">Button</Button>
+    </>
+  );
+}
+
+export default App;
