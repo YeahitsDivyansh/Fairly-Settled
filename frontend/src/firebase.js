72cf3e8ec9fbb065c63be52b9ef5fe89cb1aa9d3
prakharujn1 <128589011+prakharujn1@users.noreply.github.com>
2025-04-24 02:25:59 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +22,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth=getAuth(app);
 
-export const db=getFirestore(app);
+export const db=getFirestore(app,'fairlysettled');
+export const storage = getStorage(app);
 export default app;
